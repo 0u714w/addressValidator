686238de53c79d81a0127d16176026eb9a12cb19
Doug Enas <douglassenas@gmail.com>
2022-05-09 17:27:09 -0400

    remove fix

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -13,7 +13,7 @@
       <div class="col-md-8">
         <div class="card">
           <div class="card-header">
-            <strong>Please enter in an address hi</strong>
+            <strong>Please enter in an address</strong>
           </div>
 
           <b-form-group>
